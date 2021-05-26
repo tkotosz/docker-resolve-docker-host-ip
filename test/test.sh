fec60b68f02aff082eaaaa526f78874daff74caa
Tibor Kotosz <kotosy@gmail.com>
2021-05-26 15:15:10 +0200

    test on alpine and debian as well

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "Create docker environment..."
+docker pull tkotosz/resolve-docker-host-ip
 docker-compose up -d --remove-orphans --build
 
 echo "Check hosts file entry in container"
-docker-compose exec test-service cat /etc/hosts | grep "host.docker.internal"
+docker-compose exec test-service-alpine cat /etc/hosts | grep "host.docker.internal"
+docker-compose exec test-service-debian cat /etc/hosts | grep "host.docker.internal"
 
 echo "Destroy docker environment..."
 docker-compose down --remove-orphans
