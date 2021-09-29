3e9b0bbc6cec864a40f4efa43d0ba28676643105
Tibor Kotosz <kotosy@gmail.com>
2021-09-29 11:30:43 +0200

    Fix scripts adds unnecessary hosts entry on MAC and Windows

--- test/test.sh
+++ test/test.sh
@@ -5,8 +5,8 @@ docker pull tkotosz/resolve-docker-host-ip
 docker-compose up -d --remove-orphans --build
 
 echo "Check hosts file entry in container"
-docker-compose exec test-service-alpine cat /etc/hosts | grep "host.docker.internal"
-docker-compose exec test-service-debian cat /etc/hosts | grep "host.docker.internal"
+docker-compose exec test-service-alpine sh -c "getent hosts host.docker.internal" | grep "host.docker.internal"
+docker-compose exec test-service-debian sh -c "getent hosts host.docker.internal" | grep "host.docker.internal"
 
 echo "Destroy docker environment..."
 docker-compose down --remove-orphans
